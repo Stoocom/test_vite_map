8dc7b3be63e6abfeebee8c195881ebc15206f53e
Александр Елисеев <a.eliseev@demiusgroup.ru>
2024-12-16 10:37:36 +0300

    added style title

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,33 @@ function App() {
       <div
         style={{
           position: "absolute",
-          width: 100,
-          height: 100,
+          top: 50,
+          right: 50,
+          width: 60,
+          height: 60,
           background: "green",
+          zIndex: 2,
+          borderRadius: 50,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
-      ></div>
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+      </div>
       <Map />
     </>
   );
