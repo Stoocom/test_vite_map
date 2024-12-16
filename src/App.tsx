c4326c0f948e5d127d4d93108b8786b0945f83f9
Александр Елисеев <a.eliseev@demiusgroup.ru>
2024-12-16 11:14:08 +0300

    added style title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect } from "react";
 import "./App.css";
 import Map from "./components/Map";
 
 function App() {
+  useEffect(() => {
+    console.log("useEffect");
+    fetch("http://localhost:3010/api/auth/profile", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer " +
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzYsImxvZ2luIjoidXNlcjEiLCJpYXQiOjE3MzQxMjU0OTQsImV4cCI6MTczNTMzNTA5NH0.4stleGLtj4Wmb7Ai7YzhfFW59Hu38j7MEw0uNuPmbNo",
+      },
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+        // toast("responseData");
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }, []);
   return (
     <>
       <div
@@ -23,13 +43,13 @@ function App() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="white"
           className="size-6"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
           />
         </svg>
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/test_vite_map",
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3010',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
