6e6abc558a74e14f7fb229be7598d39508bc517b
Александр Елисеев <a.eliseev@demiusgroup.ru>
2024-12-16 08:30:16 +0300

    added vite files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./test_vite_map",
+  base: "/test_vite_map",
   plugins: [react()],
 })
