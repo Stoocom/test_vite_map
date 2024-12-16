ad2b5703a82f8b9af57049af9f2d547d56fd1358
Александр Елисеев <a.eliseev@demiusgroup.ru>
2024-12-16 14:40:16 +0300

    add events

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,6 @@
 // import React from "react";
-import { DomEvent, DomEventHandlerObject } from "@yandex/ymaps3-types";
+import { DomEvent, DomEventHandlerObject, LngLat } from "@yandex/ymaps3-types";
+import { useState } from "react";
 import {
   YMap,
   YMapComponentsProvider,
@@ -23,17 +24,21 @@ import {
 // import { features } from "./helpers";
 
 function Map() {
-  const updateHandler = (event: any) => {
-    console.log("updateHandler", event);
+  const [addLocation, setAddLocation] = useState<LngLat>([37.95, 55.65]);
+  const updateHandler = ({ location, mapInAction }: any) => {
+    console.log("location", location);
+    console.log("mapInAction", mapInAction);
+    console.log("mapInAction", mapInAction);
+    setAddLocation(location.center);
   };
 
   return (
     <div className="container">
       <YMapComponentsProvider apiKey={"c8214d76-a83c-48a0-ac30-18f3a3f2ccb0"}>
-        <YMap location={{ center: [37.95, 55.65], zoom: 10 }}>
+        <YMap location={{ center: addLocation, zoom: 10 }}>
           <YMapDefaultSchemeLayer />
           <YMapDefaultFeaturesLayer />
-          <YMapDefaultMarker coordinates={[37.95, 55.65]} />
+          <YMapDefaultMarker coordinates={addLocation} />
           <YMapControls position="bottom">
             <YMapZoomControl />
           </YMapControls>
