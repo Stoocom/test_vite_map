5b7503554844ee7c32436f5e5b5fa202ee479429
Александр Елисеев <a.eliseev@demiusgroup.ru>
2024-12-24 20:36:22 +0300

    changed add image

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import {
   getMarkers,
   toggleOpenAddMarker,
 } from "./services/store/slices/markersSlice";
-import { MdOutlineAddLocationAlt } from "react-icons/md";
+// import { MdOutlineAddLocationAlt } from "react-icons/md";
 import { RiLoginCircleLine } from "react-icons/ri";
-// import { FaRegCircleUser } from "react-icons/fa6";
+import { FaRegCircleUser } from "react-icons/fa6";
 import ModalWrapper from "./components/ModalWrapper";
 import Form from "./components/Form";
 import { ToastContainer } from "react-toastify";
@@ -53,11 +53,7 @@ function App() {
           }}
           onClick={() => dispatch(toggleOpenLoginForm())}
         >
-          <img
-            src="https://downloader.disk.yandex.ru/preview/4f80fed2873463ff2b31162cda7ba2d9e19fab3ed6e73faa88cfdd449b8391b5/inf/gJ9KiimGoYbGCe2IrDWcVGycE3C0fjH29HfwRtDDKVygSgSgsryLElCdDXXAJj1BBccTjprd_uY3WWhDvvyBVA%3D%3D?uid=1022693374&filename=%D0%97%D0%B8%D0%BC%D0%B0.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1022693374&tknv=v2&size=XXXS&crop=0"
-            alt="no"
-          />
-          {/* <FaRegCircleUser size={31} color="white" title="New" /> */}
+          <FaRegCircleUser size={31} color="white" title="New" />
         </div>
       ) : (
         <div
@@ -98,7 +94,11 @@ function App() {
           dispatch(changeZoomCurrentLocation(19));
         }}
       >
-        <MdOutlineAddLocationAlt size={36} color="white" />
+        <img
+          src="https://downloader.disk.yandex.ru/preview/4f80fed2873463ff2b31162cda7ba2d9e19fab3ed6e73faa88cfdd449b8391b5/inf/gJ9KiimGoYbGCe2IrDWcVGycE3C0fjH29HfwRtDDKVygSgSgsryLElCdDXXAJj1BBccTjprd_uY3WWhDvvyBVA%3D%3D?uid=1022693374&filename=%D0%97%D0%B8%D0%BC%D0%B0.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1022693374&tknv=v2&size=XXXS&crop=0"
+          alt="no"
+        />
+        {/* <MdOutlineAddLocationAlt size={36} color="white" /> */}
         {/* <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
